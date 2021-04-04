267e3ed7f3d977e1225bc710ddce5b87d032c586
Raihan Ryandhika <raihan.ryandhika24@gmail.com>
2021-04-04 19:20:00 +0700

    update

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import json
 app=Flask(__name__)
 mongo = PyMongo(app, uri="mongodb://coworking:admin@117.53.44.15:30001,117.53.44.15:30002,117.53.44.15:30003/coworking?authSource=coworking&replicaSet=my-mongo-set")
 collection = mongo.db.coworking
+collection2 = mongo.db.invoice
 
 @app.route("/coworking/get_coworking", methods=["GET"])
 def get_all_data():
@@ -15,7 +16,6 @@ def get_all_data():
 
 @app.route("/coworking/get_coworking/<id>", methods=["GET"])
 def get__one_data(id):
-    search = request.args.get('search')
     resps = dumps(collection.find({"_id":ObjectId(id)}))
     return resps
 
@@ -47,6 +47,56 @@ def update_data(id):
     
     except:
         return "Mohon Isi Semua Data Untuk Menjalankan Update!"
+
+# =========================== INVOICE ============================== #
+
+@app.route("/invoice/insert_invoice", methods=["POST"])
+def insert_invoice():
+    invoice = request.get_json() or {
+            'id_user': request.form['id_user'],
+            'status': request.form['status'],
+            'token': request.form['token'],
+            'tempat': request.form['tempat'],
+            'capacity': request.form['capacity'],
+            'invoice': request.form['invoice'],
+            'is_aktif': request.form['is_aktif']
+        }
+    collection2.insert(invoice)
+    return "Succes Inserting"
+
+@app.route("/invoice/get_invoice", methods=["GET"])
+def get_all_invoice():
+    resps = dumps(collection2.find({}))
+    return resps
+
+@app.route("/invoice/get_invoice/<id>", methods=["GET"])
+def get_invoice_byid(id):
+    resps = dumps(collection2.find({"_id":ObjectId(id)}))
+    return resps
+
+@app.route("/invoice/update_invoice/<id>", methods=["PUT"])
+def update_invoice(id):
+    try:
+        invoice = request.get_json() or {
+            'id_user': request.form['id_user'],
+            'status': request.form['status'],
+            'token': request.form['token'],
+            'tempat': request.form['tempat'],
+            'capacity': request.form['capacity'],
+            'invoice': request.form['invoice'],
+            'is_aktif': request.form['is_aktif']
+        }
+
+        collection2.update_many(
+            {"_id":ObjectId(id)},
+            {"$set": invoice}
+        )
+
+        return "Data Berhasil di Update!"
+    
+    except:
+        return "Mohon Isi Semua Data Untuk Menjalankan Update!"
+
 
 if "__main__" == __name__:
     app.run(host='127.0.0.1', port=5000, debug=True)
